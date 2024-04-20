9ccd90ee1c293959a0eca4e400d06d8d3b95f3c5
Seyi0 <makanjuolaseyi560@gmail.com>
2024-04-20 21:55:22 +0100

    :sparkles:

---
+++ 2/welcome.py
@@ -0,0 +1,11 @@
+line01 = "********************" #Header/footer
+line02 = "*                  *" #re-use
+line03 = "*     WELCOME!     *"
+
+print('') # start with an empty line
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
+
