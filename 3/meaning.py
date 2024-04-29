849080dd1db68df4302dcb2d5244f3b49832e3e4
Seyi0 <makanjuolaseyi560@gmail.com>
2024-04-29 13:41:54 +0100

    :sparkles:

---
+++ 3/meaning.py
@@ -0,0 +1,11 @@
+meaning = 22
+print ('')
+
+# if meaning > 10:
+#     print ('Sharp!')
+# else:
+#     print('No way')
+
+
+#Ternary Operators
+print ('Right on!') if meaning > 10 else print('Not today')
